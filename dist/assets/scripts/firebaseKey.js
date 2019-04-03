58b3ccd6a026f72091271b12a5b9babea9035652
MILA <milallantoy25@gmail.com>
2019-04-02 23:55:44 -0500

    la base

---
+++ dist/assets/scripts/firebaseKey.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDHhz0uS_l1TmFCEIJbVRT8bfMDZYJAbqw",
+  authDomain: "coderecruiter-20529.firebaseapp.com",
+  databaseURL: "https://coderecruiter-20529.firebaseio.com",
+  projectId: "coderecruiter-20529",
+  storageBucket: "",
+  messagingSenderId: "672941780418"
+};
+firebase.initializeApp(config);
+
